04ff3a0b76dd50c414d450f5234a1ad08cf884e9
Ronald Eekelder <info@oakrefactory.com>
2021-12-17 20:03:54 +0100

    Day 6

--- AdventOfCode/Day6/Day6.fs
+++ AdventOfCode/Day6/Day6.fs
@@ -3,7 +3,47 @@
 open System.IO
 open System
 
-let allInput = File.ReadLines(@"Day6\\Day6Input.txt")
+let allInput = 
+    File.ReadLines(@"Day6\\Day6Input.txt")
+    |> Seq.toList
+    |> List.head
+let initialFishes = 
+    allInput.Split(",")
+    |> Array.map int64
+let summaryFish = [|
+        initialFishes |> Array.filter (fun f -> f = 0) |> Array.length |> int64
+        initialFishes |> Array.filter (fun f -> f = 1) |> Array.length |> int64
+        initialFishes |> Array.filter (fun f -> f = 2) |> Array.length |> int64
+        initialFishes |> Array.filter (fun f -> f = 3) |> Array.length |> int64
+        initialFishes |> Array.filter (fun f -> f = 4) |> Array.length |> int64
+        initialFishes |> Array.filter (fun f -> f = 5) |> Array.length |> int64
+        initialFishes |> Array.filter (fun f -> f = 6) |> Array.length |> int64
+        initialFishes |> Array.filter (fun f -> f = 7) |> Array.length |> int64
+        initialFishes |> Array.filter (fun f -> f = 8) |> Array.length |> int64
+    |]
 
-let answer = allInput |> Seq.toList |> List.length
+
+let birthRateInDays = 7
+let daysBeforFirstCycle = 2
+let fishAfterDay fish = if (fish > 0) then (fish - 1) else birthRateInDays - 1
+
+let rec getFishes (summaryFish:int64[]) totalDays day = 
+    let summaryFishAfterDay = [|
+        summaryFish[1]
+        summaryFish[2]
+        summaryFish[3]
+        summaryFish[4]
+        summaryFish[5]
+        summaryFish[6]
+        summaryFish[7] + summaryFish[0]
+        summaryFish[8]
+        summaryFish[0]
+    |]
+    if day = totalDays 
+    then summaryFishAfterDay
+    else getFishes summaryFishAfterDay totalDays (day + 1)
+
+let answer = 
+    getFishes summaryFish 256 1 
+    |> Array.sum
 
