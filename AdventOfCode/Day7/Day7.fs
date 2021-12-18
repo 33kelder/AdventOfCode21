e5fd19b93239806c96fbe3ba96fba5666180dc53
Ronald Eekelder <info@oakrefactory.com>
2021-12-18 15:16:35 +0100

    Day 7

---
+++ AdventOfCode/Day7/Day7.fs
@@ -0,0 +1,37 @@
+﻿module Day7
+
+open System.IO
+open System
+
+let allInput = 
+    File.ReadLines(@"Day7\\Day7Input.txt")
+    |> Seq.toList
+    |> List.head
+let initialPositions = 
+    allInput.Split(",")
+    |> Array.map int
+
+let minPosition = initialPositions |> Array.min
+let maxPosition = initialPositions |> Array.max
+let nrOfCrabs = initialPositions.Length
+
+let getFuelCost i =
+    initialPositions
+    |> Array.map (fun p -> 
+            let steps = Math.Abs(p - i)
+            [|1 .. steps|] |> Array.sum
+        )
+    |> Array.sum
+
+let getMinimumFuelCost (minFuelCost, minFuelCostPosition) position =
+    let fuelCostAtPosition = getFuelCost position
+    if (fuelCostAtPosition < minFuelCost)
+    then (fuelCostAtPosition, position)
+    else (minFuelCost, minFuelCostPosition)
+
+let (fuelCost, position) =
+    [|0 .. maxPosition - 1|]
+    |> Array.fold getMinimumFuelCost (Int32.MaxValue, 0)
+
+let answer = fuelCost
+
